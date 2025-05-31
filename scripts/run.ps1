1a5a8bc9ffd48a4b163916cdff70b903d94b9def
Kyrylo Karunnyi <kyrylo.karunnyi@student.tuke.sk>
2025-05-31 04:49:28 +0200

    add mongodb compose file

---
+++ scripts/run.ps1
@@ -0,0 +1,34 @@
+param (
+    $command
+)
+
+if (-not $command)  {
+    $command = "start"
+}
+
+$ProjectRoot = "${PSScriptRoot}/.."
+
+$env:AMBULANCE_API_ENVIRONMENT="Development"
+$env:AMBULANCE_API_PORT="8080"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+switch ($command) {
+    "start" {
+         try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/ambulance-api-service
+         } finally {
+            mongo down
+         }
+    }
+   "mongo" {
+        mongo up
+    }
+    "openapi" {
+        docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    default {
+        throw "Unknown command: $command"
+    }
+}
